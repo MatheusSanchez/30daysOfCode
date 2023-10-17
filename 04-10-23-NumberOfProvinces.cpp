76a0466752514f67f8a09b67466bb97d31d6d781
Matheus Sanchez <matheus_sanchez@usp.br>
2023-10-16 22:46:07 -0300

    including unionfind solution

--- 04-10-23-NumberOfProvinces.cpp
+++ 04-10-23-NumberOfProvinces.cpp
@@ -9,6 +9,9 @@
     Time: O(V^2)
     Space: O(V)
 
+    UnionFind:
+    Time: O(log n)
+    Space: O(n)
 */
 
 class Solution {
@@ -70,5 +73,57 @@ class Solution {
                 dfs(i, graph);
             }
         }
+    }
+};
+class Solution {
+
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        
+        vector<int> parents(isConnected.size());
+
+        for(int i = 0; i < parents.size(); i++){
+            parents[i] = i;
+        }
+
+        for(int i = 0; i < isConnected.size(); i++){
+            for(int j = 0; j < isConnected.size(); j++){
+                if(isConnected[i][j] == 1){
+                    join(parents,i,j);
+                }
+            }
+        }
+
+
+        int ans = 0;
+        for(int i = 0; i < parents.size(); i++){
+            if(parents[i] == i){
+                ans++;
+            }
+        }
+       
+
+       return ans;
+        
+    }
+
+    void join(vector<int> &parents, int x, int y){
+
+        x = find(parents,x);
+        y = find(parents,y);
+
+        if(x == y){
+            return;
+        }
+
+        parents[y] = x;
+    }
+
+    int find(vector<int> &parents, int x){ 
+        if(parents[x] == x){
+            return x;
+        }
+
+        return (parents[x] = find(parents, parents[x]));
     }
 };
